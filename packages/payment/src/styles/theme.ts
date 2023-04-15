954f5ae1c7ee63adc93f902868ce57b2f47745bb
JASUN LEE <liswktjs@gmail.com>
2023-04-15 18:52:22 +0900

    feat: theme 객체 생성

---
+++ packages/payment/src/styles/theme.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  PRIMARY: ' #2AC1BC',
+  PRIMARY_SOFT: '##04C09E',
+  GRAY_600: '#D2D2D2',
+  GRAY_500: '#E5E5E5',
+  GRAY_400: '#94DACD',
+  GRAY_300: '#ECEBF1',
+  GRAY_200: '#dddddd',
+  GRAY_100: '#f6f6f6',
+  YELLOW_800: '#cbba64',
+  BLACK_500: '#111',
+  BLACK_400: '#525252',
+  BLACK_300: '#737373',
+  WHITE: '#fff'
+};
+
+export const zIndex = {
+  DRAWER: 500,
+  TOOLTIP_BOX: 400,
+  HEADER: 300
+};
+
+export const theme = {
+  colors,
+  zIndex
+} as const;
