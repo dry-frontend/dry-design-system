f308277ade250dd9988544e45e315705b07de68e
JASUN LEE <liswktjs@gmail.com>
2023-04-15 18:53:03 +0900

    feat : ThemeProvider 통해 app과 storybook에 theme 주입

--- packages/payment/src/main.tsx
+++ packages/payment/src/main.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/theme';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
