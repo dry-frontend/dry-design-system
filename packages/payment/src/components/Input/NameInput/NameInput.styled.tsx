9624281ad6988b058cb3720b9854ac60a73caede
JASUN LEE <liswktjs@gmail.com>
2023-05-06 18:50:20 +0900

    feat: 이름을 입력할 수 있는 Input 컴포넌트 구현

---
+++ packages/payment/src/components/Input/NameInput/NameInput.styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Container, Input } from '../Input.styled';
+
+export const NameContainer = styled(Container)`
+  width: 100%;
+`;
+
+export const NameInput = styled(Input)`
+  width: 100%;
+`;
