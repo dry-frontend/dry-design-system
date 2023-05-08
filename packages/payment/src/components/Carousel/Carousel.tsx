8f2fd3b877d1d672bce899e716ab6e22dfede189
전소이 <soyi.jeon@woowahan.com>
2023-05-08 23:51:24 +0900

    feat: CarouselProps 수정 및 key 추가
    props onClickPrevButton, onClickNextButton 추가

--- packages/payment/src/components/Carousel/Carousel.tsx
+++ packages/payment/src/components/Carousel/Carousel.tsx
@@ -1,28 +1,35 @@
 import React, { useState } from 'react';
 import * as S from './Carousel.styled';
 
-interface CarouselProps extends React.PropsWithChildren {}
+interface CarouselProps extends React.PropsWithChildren {
+  onClickPrevButton?: React.MouseEventHandler<HTMLButtonElement>;
+  onClickNextButton?: React.MouseEventHandler<HTMLButtonElement>;
+}
 
-const Carousel = ({ children }: CarouselProps) => {
+const Carousel = ({ children, onClickPrevButton, onClickNextButton }: CarouselProps) => {
   const [index, setIndex] = useState(0);
   const childrenCount = React.Children.count(children);
 
-  const handleClickPrev: React.MouseEventHandler<HTMLButtonElement> = () => {
+  const handleClickPrev: React.MouseEventHandler<HTMLButtonElement> = e => {
     setIndex(prev => {
       if (prev === 0) {
         return prev;
       }
       return prev - 1;
     });
+
+    onClickPrevButton && onClickPrevButton(e);
   };
 
-  const handleClickNext: React.MouseEventHandler<HTMLButtonElement> = () => {
+  const handleClickNext: React.MouseEventHandler<HTMLButtonElement> = e => {
     setIndex(prev => {
       if (prev === childrenCount - 1) {
         return prev;
       }
       return prev + 1;
     });
+
+    onClickNextButton && onClickNextButton(e);
   };
 
   return (
@@ -30,8 +37,8 @@ const Carousel = ({ children }: CarouselProps) => {
       <S.LeftButton onClick={handleClickPrev} />
       <S.ContentBackground>
         <S.ContentList index={index} totalCount={childrenCount}>
-          {React.Children.toArray(children).map(item => (
-            <li>{item}</li>
+          {React.Children.toArray(children).map((item, index) => (
+            <li key={index}>{item}</li>
           ))}
         </S.ContentList>
       </S.ContentBackground>
