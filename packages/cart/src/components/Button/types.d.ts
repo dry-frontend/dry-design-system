f84d00040f09ddf5fd1241752f1fff9b16c7be3d
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-05-12 10:09:49 +0900

    refactor: padding 객체 관리를 확장성 있도록 변경

--- packages/cart/src/components/Button/types.d.ts
+++ packages/cart/src/components/Button/types.d.ts
@@ -1,7 +1,7 @@
 import { ButtonPadding, ButtonSizes, ButtonStyles } from './styles';
 
 export declare type ButtonVariant = keyof typeof ButtonStyles.Type;
-export declare type ButtonSize = keyof typeof ButtonPadding.plain;
-export declare type ButtonAffix = keyof typeof ButtonPadding.plain.large;
+export declare type ButtonSize = keyof typeof ButtonPadding;
+export declare type ButtonAffix = keyof typeof ButtonPadding.large;
 
 export declare type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
