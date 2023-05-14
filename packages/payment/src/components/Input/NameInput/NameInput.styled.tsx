c81dc0bb7ea4e07521a5dd96e9884733ff388920
JASUN LEE <liswktjs@gmail.com>
2023-05-14 21:55:33 +0900

    fix: name padding값 수정

--- packages/payment/src/components/Input/NameInput/NameInput.styled.tsx
+++ packages/payment/src/components/Input/NameInput/NameInput.styled.tsx
@@ -3,6 +3,7 @@ import { Container, Input } from '../Input.styled';
 
 export const NameContainer = styled(Container)`
   width: 100%;
+  padding: 10px;
 `;
 
 export const NameInput = styled(Input)`
