484ba405df7c486504b463ef9432c38ee5f3155c
JASUN LEE <liswktjs@gmail.com>
2023-05-14 21:04:44 +0900

    feat: dimmer 클릭시 실행시킬 이벤트를 Props로 전달받기

--- packages/payment/src/components/Drawer/Drawer.tsx
+++ packages/payment/src/components/Drawer/Drawer.tsx
@@ -4,11 +4,17 @@ import * as S from './Drawer.styled';
 export interface DrawerProps {
   isOpen: boolean;
   anchor: 'left' | 'right' | 'top' | 'bottom';
+  handleDimmerClickEvent?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-const Drawer = ({ isOpen, anchor, children }: PropsWithChildren<DrawerProps>) => {
+const Drawer = ({
+  isOpen,
+  anchor,
+  handleDimmerClickEvent,
+  children
+}: PropsWithChildren<DrawerProps>) => {
   return (
-    <S.Dimmed className={isOpen ? 'is-open' : ''}>
+    <S.Dimmed className={isOpen ? 'is-open' : ''} onClick={handleDimmerClickEvent}>
       <S.Container className={isOpen ? 'is-open' : ''} anchor={anchor}>
         {children}
       </S.Container>
