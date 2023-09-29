ca6085d49c672bb207f397bed469fb004fd251c0
airman5573 <rpf5573@kakao.com>
2023-09-29 18:05:33 +0900

    fix: remove declare

--- packages/cart/src/components/button/types.ts
+++ packages/cart/src/components/button/types.ts
@@ -1,7 +1,7 @@
 import { ButtonPadding, ButtonStyles } from './styles';
 
-export declare type ButtonVariant = keyof typeof ButtonStyles.Type;
-export declare type ButtonSize = keyof typeof ButtonPadding;
-export declare type ButtonAffix = keyof typeof ButtonPadding.large;
+export type ButtonVariant = keyof typeof ButtonStyles.Type;
+export type ButtonSize = keyof typeof ButtonPadding;
+export type ButtonAffix = keyof typeof ButtonPadding.large;
 
-export declare type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+export type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
